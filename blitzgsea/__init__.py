15c81fbdfb85b0af67e0dfe95029a70c5873a7a6
Alexander Lachmann <alexander.lachmann@gmail.com>
2021-11-08 13:02:23 -0500

    revert precision

--- blitzgsea/__init__.py
+++ blitzgsea/__init__.py
@@ -196,15 +196,15 @@ def probability(signature, signature_map, gene_set, f_alpha_pos, f_beta_pos, f_a
         if prob_two_tailed == 1:
             nes = 0
         else:
-            #nes = norm.ppf(1-np.min([1,prob_two_tailed]))
-            nes = mpsci.distributions.normal.invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
+            nes = norm.ppf(1-np.min([1,prob_two_tailed]))
+            #nes = mpsci.distributions.normal.invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
         pval = 2*prob_two_tailed
     else:
         rv = gamma(neg_alpha, scale=neg_beta, loc=0)
         prob = rv.cdf(-es)
         prob_two_tailed = np.min([0.5,(1-np.min([prob*(1-pos_ratio)+pos_ratio,1]))])
-        #nes = norm.ppf(np.min([1,prob_two_tailed]))
-        nes = mpsci.distributions.normal.invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
+        nes = norm.ppf(np.min([1,prob_two_tailed]))
+        #nes = mpsci.distributions.normal.invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
         pval = 2*prob_two_tailed
 
     return gsize, es, nes, pval
