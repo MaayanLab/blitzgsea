e8f3ad12b76805ff76336029994d8eeb260de7a4
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-01-06 17:29:08 -0500

    legenes

--- blitzgsea/__init__.py
+++ blitzgsea/__init__.py
@@ -198,7 +198,7 @@ def probability(signature, signature_map, gene_set, f_alpha_pos, f_beta_pos, f_a
     
     rs, es = enrichment_score(signature, signature_map, gene_set)
     legenes = get_leading_edge(rs, signature, gene_set, signature_map)
-
+    print(legenes)
     pos_alpha = f_alpha_pos(gsize)
     pos_beta = f_beta_pos(gsize)
 
@@ -223,7 +223,7 @@ def probability(signature, signature_map, gene_set, f_alpha_pos, f_beta_pos, f_a
 
     return gsize, es, nes, pval, legenes
 
-def gsea(signature, library, permutations: int=2000, anchors: int=20, min_size: int=5, max_size: int=np.inf, processes: int=4, plotting: bool=False, verbose: bool=False, symmetric: bool=False, legenes: bool=False, seed: int=0):
+def gsea(signature, library, permutations: int=2000, anchors: int=20, min_size: int=5, max_size: int=np.inf, processes: int=4, plotting: bool=False, verbose: bool=False, symmetric: bool=False, seed: int=0):
     if seed == -1:
         seed = random.randint(-10000000, 100000000)
     
