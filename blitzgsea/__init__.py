d7c0545ca12f4141477c3b9718ff547ddaadc491
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-01-08 14:05:32 -0500

    100

--- blitzgsea/__init__.py
+++ blitzgsea/__init__.py
@@ -204,13 +204,12 @@ def probability(signature, abs_signature, signature_map, gene_set, f_alpha_pos, 
     pos_beta = f_beta_pos(gsize)
     pos_ratio = f_pos_ratio(gsize)
 
-    mp.dps = 200
-    mp.prec = 200
+    mp.dps = 100
+    mp.prec = 100
 
     if es > 0:
         prob = gamma.cdf(es, float(pos_alpha), scale=float(pos_beta))
         if prob > 0.999999:
-            print(prob)
             prob = gammacdf(es, float(pos_alpha), float(pos_beta))
         prob_two_tailed = np.min([0.5,(1-np.min([(1-pos_ratio)+prob*pos_ratio,1]))])
         if prob_two_tailed == 1:
@@ -218,8 +217,9 @@ def probability(signature, abs_signature, signature_map, gene_set, f_alpha_pos, 
         else:
             nes = invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
             if nes == np.inf:
-                mp.dps = 1000
-                mp.prec = 1000
+                print(prob)
+                mp.dps = 100
+                mp.prec = 100
                 nes = invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
         pval = 2*prob_two_tailed
     else:
@@ -230,12 +230,12 @@ def probability(signature, abs_signature, signature_map, gene_set, f_alpha_pos, 
         prob_two_tailed = np.min([0.5,(1-np.min([prob*(1-pos_ratio)+pos_ratio,1]))])
         nes = invcdf(mpf(np.min([1,prob_two_tailed])))
         if nes == np.inf:
-            mp.dps = 1000
-            mp.prec = 1000
+            mp.dps = 100
+            mp.prec = 100
             nes = invcdf(mpf(1)-mpf(np.min([1,prob_two_tailed])))
         pval = 2*prob_two_tailed
-    mp.dps = 200
-    mp.prec = 200
+    mp.dps = 100
+    mp.prec = 100
     return gsize, es, nes, pval, legenes
 
 def gsea(signature, library, permutations: int=2000, anchors: int=20, min_size: int=5, max_size: int=np.inf, processes: int=4, plotting: bool=False, verbose: bool=False, symmetric: bool=True, seed: int=0):
