3152d5420aadde4fa726de8bc173b60c82aa0c0f
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2023-05-08 14:29:03 -0400

    fix range

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="blitzgsea",
-    version="1.3.26",
+    version="1.3.27",
     author="Alexander Lachmann",
     author_email="alexander.lachmann@mssm.edu",
     description="Package for fast calculation of GSEA similar to prerank using gamma distribution approximation.",
