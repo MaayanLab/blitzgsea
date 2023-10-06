ab5bb416e113dc4923c0c2f5df3f999c795d3bd7
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2023-10-06 16:40:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="blitzgsea",
-    version="1.3.34",
+    version="1.3.35",
     author="Alexander Lachmann",
     author_email="alexander.lachmann@mssm.edu",
     description="Package for fast and accurate calculation of Gene Set Enrichment Analysis (GSEA) similar to prerank using gamma distribution approximation.",
