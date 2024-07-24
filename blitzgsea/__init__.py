fix numerical instability
mapping values close to 1 to float caused numerical instability.

Higher numeric stability even for very small p-values should now work properly.

blitzgsea/__init__.py
@@ -338,21 +338,26 @@ def gsea(signature, library, permutations: int=1000, anchors: int=20, min_size: 
                 if prob > 0.999999999 or prob < 0.00000000001:
                     mp.dps = deep_accuracy
                     mp.prec = deep_accuracy
-                    prob = gammacdf(es, float(pos_alpha), float(pos_beta))
+                    prob = gammacdf(es, float(pos_alpha), float(pos_beta), dps=deep_accuracy)
                 prob_two_tailed = np.min([0.5,(1-np.min([prob*pos_ratio+1-pos_ratio,1]))])
-                if prob_two_tailed == 1:
-                    nes = 0
-                else:
-                    nes = invcdf(1-np.min([1,prob_two_tailed]))
+                nes = invcdf(1-np.min([1,prob_two_tailed]))
                 pval = 2*prob_two_tailed
             else:
                 prob = gamma.cdf(-es, float(pos_alpha), scale=float(pos_beta))
                 if prob > 0.999999999 or prob < 0.00000000001:
                     mp.dps = deep_accuracy
                     mp.prec = deep_accuracy
-                    prob = gammacdf(-es, float(pos_alpha), float(pos_beta))
+                    prob = gammacdf(-es, float(pos_alpha), float(pos_beta), dps=deep_accuracy)
                 prob_two_tailed = np.min([0.5,(1-np.min([(((prob)-(prob*pos_ratio))+pos_ratio),1]))])
+                if prob_two_tailed == 0.5:
+                    #print("help")
+                    prob_two_tailed = prob_two_tailed-prob
+
                 nes = invcdf(np.min([1,prob_two_tailed]))
+
+                if nes == 0:
+                    print("Fixy", k, es, prob, prob_two_tailed, gsize)
+
                 pval = 2*prob_two_tailed
             
             mp.dps = accuracy

blitzgsea/mpsci.py
@@ -4,18 +4,19 @@ from mpmath import mp, exp, log
 import math
 from scipy.stats import norm, gamma
 
-def gammacdf(x, k, theta):
+def gammacdf(x, k, theta, dps=100):
     """
     Gamma distribution cumulative distribution function.
     k is the shape parameter
     theta is the scale parameter (reciprocal of the rate parameter)
     Unlike scipy, a location parameter is not included.
     """
+    mp.dps = dps
     with mp.extradps(mp.dps):
         x = mp.mpf(x)
         if x < 0:
             return mp.zero
-        return float(mp.gammainc(k, 0, x/theta, regularized=True))
+        return mp.gammainc(k, 0, x/theta, regularized=True)
 
 def gammacdf1(x, k, theta):
     log_sf_value = gamma.logsf(x, k, scale=theta)
@@ -50,9 +51,15 @@ def invcdf_old(p, mu=0, sigma=1):
         return x
 
 def invcdf(p, mu=0, sigma=1):
+    orig_p = p
+    if p > 0.5:
+        p = 1- p
     p = float(p)
     if math.isnan(p):
         p = 1
     p = min(max(p, 0), 1)
     n = norm.isf(p)
+
+    if orig_p > 0.5:
+        return -n
     return n